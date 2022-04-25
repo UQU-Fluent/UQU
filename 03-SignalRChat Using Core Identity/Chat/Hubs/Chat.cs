964ffb1a0b8fe8938c8ddb260afafa885a490bf9
elgendy5610151@outlook.com <elgendy5610151@outlook.com>
2022-04-25 19:32:22 +0300

    UQU Tasks

---
+++ 03-SignalRChat Using Core Identity/Chat/Hubs/Chat.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Chat.Hubs
+{
+    public class Chat : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            // Context.User.Identity.Name gives the user name of the Identity based on the current user session.
+            await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name ?? "anonymous", message);
+        }
+    }
+}
